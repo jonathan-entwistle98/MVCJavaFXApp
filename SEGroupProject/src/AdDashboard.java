46b851039e51f16227bbf492a7c6495f87d82a0a
Jonathan <Jonathan@152.78.250.204>
2018-02-27 20:54:05 +0000

    Created new "View" Class, attempting to split view from main method, not current working

--- SEGroupProject/src/AdDashboard.java
+++ SEGroupProject/src/AdDashboard.java
@@ -10,39 +10,53 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class AdDashboard extends Application{
+public class AdDashboard{
+	
+	private Controller controller;
 	
 	public static void main(String[] args) {
-		launch(args);
+		
+		 AdDashboard main = new AdDashboard();
+		 main.init(args);
+		 
+	//	launch(args);
 	}
+
 
 	
 	/**
 	 * Creates a new Scene, adds this scene to the stage and sets the css and fxml files to be used.
 	 * Controller and Model classes instantiated, and reference to model passed to controller
 	 */
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void init(String[] args) {
+		View view = new View();
+		Application.launch(View.class, args);
+		
+//		View view = new View(primaryStage);
 			
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("viewDashboard.fxml"));
+	//	FXMLLoader loader = new FXMLLoader(getClass().getResource("viewDashboard.fxml"));
 		
-		Controller controller = new Controller();
+		controller = new Controller();
 		
-		loader.setController(controller);
+//		FXMLLoader loader = view.getLoader();
 		
-		Parent root = loader.load();
+		view.setController(controller);
+		
+		
+		
+		//view.setRoot(root);
 		
 		Model model = new Model();
 		model.loadCSVData();
 		
 		controller.initModel(model);	
 
-		primaryStage.setTitle("Dashboard");
-		Scene scene = new Scene(root, 1000, 600);
-		scene.getStylesheets().add("style.css");
-		primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("icon.png")));
-		primaryStage.setScene(scene);
-		primaryStage.show();
+//		primaryStage.setTitle("Dashboard");
+//		Scene scene = new Scene(root, 1000, 600);
+//		scene.getStylesheets().add("style.css");
+//		primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("icon.png")));
+//		primaryStage.setScene(scene);
+//		primaryStage.show();
 		
 	}
 	
