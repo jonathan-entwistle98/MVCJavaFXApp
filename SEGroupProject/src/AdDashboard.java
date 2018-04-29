fc4c25e4949614efc0ea732625ef028b6cf8c6da
Maanasa <maanasakrishna20@gmail.com>
2018-04-29 22:29:04 +0100

    added some stuff

--- SEGroupProject/src/AdDashboard.java
+++ SEGroupProject/src/AdDashboard.java
@@ -65,6 +65,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 import javafx.util.Callback;
 
 public class AdDashboard extends Application{
@@ -625,7 +626,6 @@ public class AdDashboard extends Application{
 		ObservableList<String> campaignChoices = FXCollections.observableArrayList(campaignNames);
 		selectCampaignChoiceBox.setItems(campaignChoices);
 		
-		appearanceMenuItem.setDisable(true);
 		terminologyMenuItem.setDisable(true);
 		aboutMenuItem.setDisable(true);
 		
@@ -766,6 +766,45 @@ public class AdDashboard extends Application{
 		selectCampaignChoiceBox.setItems(campaignChoices);
 		
 	}
+	
+	public void resetDefaultsClicked(){
+		
+	}
+	
+	public void applyChangesClicked(){
+		
+	}
+	
+public void customizeAppearanceClicked() throws IOException{
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("appearanceView.fxml"));
+        loader.setController(this);
+        Parent appRoot = loader.load();
+        
+        Stage appStage = new Stage();
+        
+        appStage.setTitle("Appearance");
+        
+        Scene appScene = new Scene(appRoot,800,400);
+        
+        appStage.setScene(appScene);
+        appStage.initModality(Modality.WINDOW_MODAL);
+        appStage.initOwner(stage);
+        appStage.show();
+        graphViewBorderPane.setDisable(true);
+        
+        appStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+             @Override
+             public void handle(WindowEvent event) {
+                 graphViewBorderPane.setDisable(false);
+             }
+         });
+
+        
+        
+    }
+    
 	
 	public void loadNewCampaignClicked(){
 
